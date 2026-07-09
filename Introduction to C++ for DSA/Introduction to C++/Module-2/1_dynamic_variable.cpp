0bdb6026e2410cd1558dc4e861de5fc6a1a943a1
Nahid-2024331523 <bashirshahriernahid@gmail.com>
2026-07-09 22:01:55 +0600

    module-2

---
+++ Introduction to C++ for DSA/Introduction to C++/Module-2/1_dynamic_variable.cpp
@@ -0,0 +1,50 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int *p=new int;
+//     *p=100;
+//     cout<<*p;
+//     return 0;
+// }
+
+
+
+// //Memory Delete
+// #include<bits/stdc++.h>
+// using namespace std;
+// int *p;
+// void fun()
+// {
+//     int x=10;
+//     p=&x;
+//     cout<<"fun-> "<<*p<<endl;
+//     return;
+// }
+// int main()
+// {
+//     fun();
+//     cout<<"main-> "<<*p<<endl;    
+//     return 0;
+// }
+
+
+
+// //Mwmory Delete Solve
+// #include<bits/stdc++.h>
+// using namespace std;
+// int *p;
+// void fun()
+// {
+//     int *x=new int;
+//     *x=10;
+//     p=x;
+//     cout<<"fun-> "<<*p<<endl;
+//     return;
+// }
+// int main()
+// {
+//     fun();
+//     cout<<"main-> "<<*p<<endl;    
+//     return 0;
+// }
